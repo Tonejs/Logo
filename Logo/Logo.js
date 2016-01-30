46e4948fe9c96c7b8794ae972bdacd7ab068f341
Yotam Mann <yotammann@gmail.com>
2016-01-30 12:36:09 -0500

    removing jquery

--- Logo/Logo.js
+++ Logo/Logo.js
@@ -1,10 +1,12 @@
-define(["jquery", "Logo.scss", "Waveforms"], function($, LogoStyle, Waveforms){
+define(["Logo.scss", "Waveforms"], function(LogoStyle, Waveforms){
 
 	var bufferLen = 256;
 
 	var waveform = Waveforms(bufferLen).random;
 
-	var hasTone = typeof window.Tone === "function";
+	function hasTone(){
+		return typeof window.Tone === "function";
+	}
 
 	/**
 	 *  @class  Logo visualizes current Tone.js context
@@ -23,47 +25,56 @@ define(["jquery", "Logo.scss", "Waveforms"], function($, LogoStyle, Waveforms){
 		 *  The container element
 		 *  @type  {Element}
 		 */
-		this.element = $("<div>", {
-			"id" : "TonejsLogo"
-		}).appendTo(options.container)
-			.on("click", function(e){
-				e.preventDefault();
-				window.location.href = "http://tonejs.org";
-			});
+		this.element = document.createElement("div");
+		this.element.id = "TonejsLogo";
+
+		if (typeof options.container === "string"){
+			document.querySelector(options.container).appendChild(this.element);
+		} else if (options.container instanceof Element){
+			options.container.appendChild(this.element);
+		}
 
 
 		/**
 		 *  the Tone.js title
 		 *  @type  {Element}
 		 */
-		this.textContainer = $("<div>", {
-			"id" : "TextContainer",
-		}).appendTo(this.element);
+		this.textContainer = document.createElement("div");
+		this.textContainer.id = "TextContainer";
+		this.element.appendChild(this.textContainer);
 
 		/**
 		 *  the waveform canvas
 		 *  @type  {Element}
 		 */
-		this.canvas = $("<canvas>", {
-			"id" : "Canvas",
-		}).appendTo(this.textContainer);
+		this.canvas = document.createElement("Canvas");
+		this.canvas.id = "Canvas";
+		this.textContainer.appendChild(this.canvas);
 
 		/**
 		 *  the drawing context
 		 *  @type  {Canavs}
 		 */
-		this.context = this.canvas.get(0).getContext("2d");
+		this.context = this.canvas.getContext("2d");
 
 		/**
 		 *  the Tone.js title
 		 *  @type  {Element}
 		 */
-		this.title = $("<div>", {
-			"id" : "Title",
-			// "text" : "Tone.js"
-		}).appendTo(this.textContainer).html("<span class='Closer'>T</span>one<span class='Closer'>.</span><span id='JS'>js</span>");
+		this.title = document.createElement("div");
+		this.title.id = "Title";
+		this.textContainer.appendChild(this.title);
+		this.title.innerHTML = "<span class='Closer'>T</span>one<span class='Closer'>.</span><span id='JS'>js</span>";
 
-		if (hasTone){
+		/**
+		 *  The link
+		 *  @type {Element}
+		 */
+		this.link = document.createElement("a");
+		this.link.href = "http://tonejs.org";
+		this.element.appendChild(this.link);
+
+		if (hasTone()){
 			/**
 			 *  The waveform analysis of the incoming signal
 			 *  @type  {Tone.Analyser}
@@ -99,7 +110,7 @@ define(["jquery", "Logo.scss", "Waveforms"], function($, LogoStyle, Waveforms){
 		//set the size
 		this.resize(options.width, options.height);
 
-		if (hasTone){
+		if (hasTone()){
 			//start the draw loop
 			this._draw();
 		} else {
@@ -126,24 +137,21 @@ define(["jquery", "Logo.scss", "Waveforms"], function($, LogoStyle, Waveforms){
 	 */
 	Logo.prototype.resize = function(width, height) {
 		//set the size of the logo
-		this.element.width(width);
-		this.element.height(height);
+		this.element.style.width = width + "px";
+		this.element.style.height = height + "px";
 		// this.canvas.width(height);
 		//double pixel density
-		this.context.canvas.width = this.canvas.height() * 2;
-		this.context.canvas.height = this.canvas.height() * 2;
+		this.context.canvas.width = this.canvas.offsetHeight * 2;
+		this.context.canvas.height = this.canvas.offsetHeight * 2;
 		//set the font size
 
-		this.title.css({
-			"line-height" : (height * 0.85).toString() + "px",
-			"font-size" : height * 0.88,
-			// "text-shadow" : "-"+textShadow+"px "+textShadow+"px #3833ED"
-		});
-		this.canvas.css({
-			"border-radius" : height/50,
-			"width" : this.canvas.height(),
-			"height" : this.canvas.height()
-		});
+		this.title.style.lineHeight = (height * 0.85).toString() + "px";
+		this.title.style.fontSize =  (height * 0.88).toString() + "px";
+
+		this.canvas.style.borderRadius = height/50 + "px";
+		this.canvas.style.width = this.canvas.offsetHeight;
+		this.canvas.style.height = this.canvas.offsetHeight;
+
 		return this;
 	};
 
@@ -240,7 +248,7 @@ define(["jquery", "Logo.scss", "Waveforms"], function($, LogoStyle, Waveforms){
 		this._signal = null;
 	};
 
-	if (hasTone){
+	if (hasTone()){
 		Tone.Logo = Logo;
 	}
 
